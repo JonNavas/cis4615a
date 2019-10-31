c21ebd6c5319636d0e2aaef65f3b8cba302ea523
JonNavas <40069322+JonNavas@users.noreply.github.com>
2019-10-31 18:46:20 -0400

    Updated code to be compliant with Java guidelines

--- R16_EVN06_J.java
+++ R16_EVN06_J.java
@@ -7,13 +7,5 @@ class Stuff
 {
   
 	private static final bool DEBUG = False;
-  
-	// Other fields and methods
-	public static void main(String args[]) 
-	{
-		Stuff.DEBUG = True;
-		Stuff stuff = new Stuff();
-		// Test stuff
-	}
 	
 }
