2ff74480ef114728a3b0c730c852cdeefa980df9
JonNavas <40069322+JonNavas@users.noreply.github.com>
2019-11-04 19:54:05 -0500

    Made code compilable

--- R16_EVN06_J.java
+++ R16_EVN06_J.java
@@ -1,11 +1,19 @@
 /******************************************************************************
  ENV06-J. Production code must not contain debugging entry points
- Non-Compliant Code
+ Compilation: javac R16_EVN06_J.java
+ Execution:   java R16_EVN06_J
+ 
+ Compliant Code
  ******************************************************************************/
 
 class Stuff 
 {
   
 	private static final bool DEBUG = False;
+	
+	public class main(String args[]) 
+	{
+   		//...
+    	}
 	
 }
